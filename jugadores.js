684ea0b8be9b59266e2e6061b9752d001c17d49f
Francisco Rodríguez <panchoxrm@gmail.com>
2026-06-24 20:53:15 -0500

    Banderas de los grupos G y K incorporadas en la carpeta banderas dentro de img

--- jugadores.js
+++ jugadores.js
@@ -24,17 +24,14 @@ function crearJugador(id, nombre, pais, posicion, urlImagen, urlBandera, colorFo
     };
 }
 
-
-
 cromosMundial.push(
     crearJugador(1, "Patrik Schick", "Chequia", "Delantero", "img/jugadores/1_Patrik_Schick.jpg", "img/banderas/chequia.jpg", "#d71920", 25, 52, "Anotó el gol más lejano en la historia de la Eurocopa (45.5 metros)."),
     crearJugador(2, "Son Heung-min", "Corea del Sur", "Delantero", "img/jugadores/2_Son_Heung.jpg", "img/banderas/coreaDelSur.jpg", "#003478", 54, 143, "Es el máximo goleador asiático en la historia de la Premier League."),
     crearJugador(3, "Hirving Lozano", "México", "Extremo", "img/jugadores/3_Hirving_Lozano.jpg", "img/banderas/mexico.jpg", "#006847", 18, 70, "Su gol contra Alemania en 2018 provocó una celebración tan grande que algunos sismógrafos registraron vibraciones en Ciudad de México."),
-    crearJugador(4, "Frenkie de Jong", "Paises Bajos", "Mediocentro", "img/jugadores/4_Frenkie_de_Jong.jpg", "img/banderas/paisesBajos.jpg", "#ff8f00", 2, 55, "Fue pieza clave en la reestructuración del Ajax que llegó a semifinales de Champions."),
+    crearJugador(4, "Frenkie de Jong", "Países Bajos", "Mediocentro", "img/jugadores/4_Frenkie_de_Jong.jpg", "img/banderas/paisesBajos.jpg", "#ff8f00", 2, 55, "Fue pieza clave en la reestructuración del Ajax que llegó a semifinales de Champions."),
     crearJugador(5, "Alexander Isak", "Suecia", "Delantero", "img/jugadores/5_Alexander_Isak.jpg", "img/banderas/suecia.jpg", "#006aa7", 16, 56, "Es el goleador más joven en la historia de la selección sueca."),
     crearJugador(6, "Wahbi Khazri", "Túnez", "Mediapunta", "img/jugadores/6_Wahbi_Khazri.jpg", "img/banderas/tunez.jpg", "#e70013", 25, 74, "Es el único jugador tunecino en anotar goles en dos Copas del Mundo distintas.")
 );
-
 
 // 3. Función de renderizado inicial usando .forEach()
 function renderizarAlbum() {
